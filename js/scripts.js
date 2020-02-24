b6482ae37993686d68c3a7e0d12610c14c1aa4e0
mwangimk <mwangikingstonemk@gmail.com>
2020-02-24 16:33:59 +0300

    mailchimp

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function myFuntion()
+{
+  var name=document.getElementById("nameDetail").value;
+  var email=document.getElementById("email").value;
+  var message=document.getElementById("massage").value;
+  alert(name+"we have received yout message. Thank you for reaching out to us.");
+};
